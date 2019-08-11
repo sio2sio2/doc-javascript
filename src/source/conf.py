489c61a130e5acbfd39736bca160eaa93bbac490
José Miguel Sánchez Alés <sio2sio2+github@gmail.com>
2019-08-11 21:25:22 +0200

    Añade la explicación sobre el formato de salida de webpack

--- src/source/conf.py
+++ src/source/conf.py
@@ -26,7 +26,7 @@ author = 'José Miguel Sánchez Alés'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
